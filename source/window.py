53362f5a8a6604ff40a06e0471be542bc3c8723f
fcortevargas <f.cortevargas@gmail.com>
2023-07-17 23:32:48 +0200

    :lipstick: Created window.py file where the main screen of the game is defined and initialized.

---
+++ source/window.py
@@ -0,0 +1,11 @@
+import pygame
+
+# Initialize PyGame
+pygame.init()
+
+# Set window's width and height
+width = 800
+height = 400
+
+# Initialize window
+screen = pygame.display.set_mode((width, height))
