3b418d645c599580b75919400bd26e91bd7abf48
fcortevargas <f.cortevargas@gmail.com>
2023-07-18 00:30:25 +0200

    :sparkles: Added functionality to close the window.

--- source/window.py
+++ source/window.py
@@ -1,4 +1,5 @@
 import pygame
+from sys import exit
 
 # Initialize PyGame
 pygame.init()
@@ -9,3 +10,13 @@ height = 400
 
 # Initialize window
 screen = pygame.display.set_mode((width, height))
+
+while True:
+    for event in pygame.event.get():
+        # If close button has been pressed, quit PyGame
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            exit()
+
+    # Draw all elements, update everything
+    pygame.display.update()
