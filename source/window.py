b9be13a76b04674a37fef0c2001b6aff95d9dd6e
fcortevargas <f.cortevargas@gmail.com>
2023-07-18 13:57:33 +0200

    ✨ Defined the game's framerate, and placed a bunch of stuff on the screen and got things to move for the first time.

--- source/window.py
+++ source/window.py
@@ -5,11 +5,50 @@ from sys import exit
 pygame.init()
 
 # Set window's width and height
-width = 800
-height = 400
+width = 1080  # px
+height = 720  # px
+
+# Set game's frame rate
+framerate = 60  # Hz
+
+# Define ground and sky ratios
+ground_ratio = 0.15
+sky_ratio = 1 - ground_ratio
 
 # Initialize window
 screen = pygame.display.set_mode((width, height))
+
+# Initialize PyGame clock
+clock = pygame.time.Clock()
+
+# Initialize title font
+title_font = pygame.font.Font('../font/Scarville-Free.otf', 150)
+
+# Set window caption "Ofrenda"
+pygame.display.set_caption('Ofrenda')
+
+# Create ground_surface
+ground_surface = pygame.Surface((width, height*ground_ratio))
+ground_surface.fill('pink4')
+
+# Create sky_surface
+sky_surface = pygame.Surface((width, height*sky_ratio))
+sky_surface.fill('plum4')
+
+# Create title_surface
+title_surface = title_font.render('Ofrenda', False, 'hotpink4')
+
+# Create and scale character
+character_surface = pygame.image.load('../graphics/characters/neutral.png')
+character_surface.convert()
+character_surface = pygame.transform.scale_by(character_surface, 0.25)
+character_width, character_height = character_surface.get_size()
+
+# Define character's position
+character_x_pos = 540
+character_y_pos = height * (1 - ground_ratio) - character_height
+
+character_x_vel = 4
 
 while True:
     for event in pygame.event.get():
@@ -18,5 +57,22 @@ while True:
             pygame.quit()
             exit()
 
+    # Blit surfaces on screen
+    screen.blit(sky_surface, (0, 0))
+    screen.blit(ground_surface, (0, height*(1-ground_ratio)))
+    screen.blit(title_surface, (340, 100))
+
+    if character_x_pos >= width - character_width and character_x_vel > 0:
+        character_x_vel *= -1
+        character_surface = pygame.transform.flip(character_surface, True, False)
+    elif character_x_pos <= 0 and character_x_vel < 0:
+        character_x_vel *= -1
+        character_surface = pygame.transform.flip(character_surface, True, False)
+
+    character_x_pos += character_x_vel
+
+    screen.blit(character_surface, (character_x_pos, character_y_pos))
+
     # Draw all elements, update everything
     pygame.display.update()
+    clock.tick(framerate)
