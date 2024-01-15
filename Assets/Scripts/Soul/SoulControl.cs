a253007417aebcfd96b4e1f06896203ab9ff0951
fcortevargas <f.cortevargas@gmail.com>
2024-01-15 21:25:04 +0100

    :art: Improved structure and formatting of the SoulControl.cs script.

--- Assets/Scripts/Soul/SoulControl.cs
+++ Assets/Scripts/Soul/SoulControl.cs
@@ -4,145 +4,130 @@ using Pathfinding;
 
 public class SoulControl : MonoBehaviour
 {
-    public GameObject player; // The player GameObject that the spirit is following.
-    public GameObject portal; // The portal GameObject that the spirit will go to if close enough.
+    public GameObject player; // The player GameObject that the spirit is following
+    public GameObject portal; // The portal GameObject that the spirit will go to if close enough
 
-    private const float nextWaypointDistance = 1f; // The minimum distance to consider a waypoint as reached.
-    private const float yDistanceToPortalThreshold = 1f; // The distance to determine if the spirit can "see" the portal.
-    private const float distanceThreshold = 5f; // The distance threshold to trigger the faster speed
+    private const float NextWaypointDistance = 1f; // The minimum distance to consider a waypoint as reached
+    private const float YDistanceToPortalThreshold = 1f; // The distance to determine if the spirit can "see" the portal
+    private const float DistanceThreshold = 5f; // The distance threshold to trigger the faster speed
 
-    private const float normalSpeed = 250; // The normal movement speed of the spirit.
-    private const float fastSpeed = 400; // The faster movement speed of the spirit.
+    private const float NormalSpeed = 250; // The normal movement speed of the spirit
+    private const float FastSpeed = 400; // The faster movement speed of the spirit
 
     [SerializeField] private float speed;
 
-    private Path path; // The calculated path for the spirit to follow.
-    private int currentWaypoint = 0; // The index of the current waypoint in the path.
-    // [SerializeField] private bool reachedEndOfPath = false; // Indicates if the spirit has reached the end of its path.
+    private Path _path; // The calculated path for the spirit to follow
+    private int _currentWaypoint; // The index of the current waypoint in the path
+    // [SerializeField] private bool reachedEndOfPath = false; // Indicates if the spirit has reached the end of its path
 
-    Seeker seeker; // A component for pathfinding calculations.
-    Rigidbody2D rb; // The Rigidbody2D component for the spirit's physics.
+    private Seeker _seeker; // A component for pathfinding calculations
+    private Rigidbody2D _rb; // The Rigidbody2D component for the spirit's physics
 
-    private Target target;
+    private Target _target;
 
     private struct Target
     {
-        public Transform transform;
-        public float distance;
-        public string type;
+        public Transform Transform;
+        public float Distance;
+        public string Type;
     }
-
-    // Start is called before the first frame update
-    void Start()
+    
+    private void Awake()
     {
-        seeker = GetComponent<Seeker>(); // Get the Seeker component attached to this GameObject.
-        rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component attached to this GameObject.
+        _seeker = GetComponent<Seeker>(); // Get the Seeker component attached to this GameObject
+        _rb = GetComponent<Rigidbody2D>(); // Get the Rigidbody2D component attached to this GameObject
 
-        // Call UpdatePath method repeatedly with a delay of 0.5 seconds.
+        // Call UpdatePath method repeatedly with a delay of 0.5 seconds
         InvokeRepeating(nameof(UpdatePath), 0f, .5f);
     }
 
-    public bool IsPortalInView()
+    private bool IsPortalInView()
     {
-        // Get the y distance between the spirit and the portal.
-        float yDistanceToPortal = Math.Abs(transform.position.y - portal.transform.position.y);
+        // Get the y distance between the spirit and the portal
+        var yDistanceToPortal = Math.Abs(transform.position.y - portal.transform.position.y);
 
-        // If that distance is small enough, then the portal is in view.
-        if (yDistanceToPortal < yDistanceToPortalThreshold)
-        {
-            return true;
-        }
-        else
-        {
-            return false;
-        }
+        // If that distance is small enough, then the portal is in view
+        return yDistanceToPortal < YDistanceToPortalThreshold;
     }
 
-    // Select the closest player ghost (left or right) for the spirit to follow or the portal if it is close enough.
+    // Select the closest player ghost (left or right) for the spirit to follow or the portal if it is close enough
     private Target SelectClosestTarget()
     {
         Target closestTarget = new();
 
-        Transform leftGhost = player.transform.Find("Left Ghost"); // Find the left player ghost.
-        Transform rightGhost = player.transform.Find("Right Ghost"); // Find the right player ghost.
+        var leftGhost = player.transform.Find("Left Ghost"); // Find the left player ghost
+        var rightGhost = player.transform.Find("Right Ghost"); // Find the right player ghost
 
-        // Calculate distances to both ghosts and the portal.
-        float distanceToLeftGhost = Vector2.Distance(transform.position, leftGhost.position);
-        float distanceToRightGhost = Vector2.Distance(transform.position, rightGhost.position);
+        // Calculate distances to both ghosts and the portal
+        var position = transform.position;
+        var distanceToLeftGhost = Vector2.Distance(position, leftGhost.position);
+        var distanceToRightGhost = Vector2.Distance(position, rightGhost.position);
 
-        // Determine the closest player ghost based on distances.
-        closestTarget.transform = (distanceToLeftGhost < distanceToRightGhost) ? leftGhost : rightGhost;
-        closestTarget.distance = Math.Min(distanceToLeftGhost, distanceToRightGhost);
-        closestTarget.type = "player";
+        // Determine the closest player ghost based on distances
+        closestTarget.Transform = (distanceToLeftGhost < distanceToRightGhost) ? leftGhost : rightGhost;
+        closestTarget.Distance = Math.Min(distanceToLeftGhost, distanceToRightGhost);
+        closestTarget.Type = "player";
 
-        float distanceToPortal = Vector2.Distance(transform.position, portal.transform.position);
+        var distanceToPortal = Vector2.Distance(transform.position, portal.transform.position);
 
         // If the portal is closer than a certain 
-        if (distanceToPortal < distanceThreshold && IsPortalInView())
-        {
-            closestTarget.transform = portal.transform;
-            closestTarget.distance = distanceToPortal;
-            closestTarget.type = "portal";
-        }
+        if (!(distanceToPortal < DistanceThreshold) || !IsPortalInView()) return closestTarget; 
+        
+        closestTarget.Transform = portal.transform;
+        closestTarget.Distance = distanceToPortal;
+        closestTarget.Type = "portal";
 
         return closestTarget;
     }
 
-    // Update the path for the spirit to follow.
-    void UpdatePath()
+    // Update the path for the spirit to follow
+    private void UpdatePath()
     {
-        target = SelectClosestTarget(); // Select the closest target.
+        _target = SelectClosestTarget(); // Select the closest target
 
-        if (seeker.IsDone()) // Check if the Seeker component is not currently calculating a path.
-            seeker.StartPath(rb.position, target.transform.position, OnPathComplete); // Start calculating a path to the player.
+        if (_seeker.IsDone()) // Check if the Seeker component is not currently calculating a path
+            _seeker.StartPath(_rb.position, _target.Transform.position, OnPathComplete); // Start calculating a path to the player
     }
 
-    // Callback function called when path calculation is complete.
-    void OnPathComplete(Path p)
+    // Callback function called when path calculation is complete
+    private void OnPathComplete(Path p)
     {
-        if (!p.error) // Check if there are no errors in the calculated path.
-        {
-            path = p; // Store the calculated path.
-            currentWaypoint = 0; // Reset the current waypoint index to the start.
-        }
+        if (p.error) return; // Check if there are no errors in the calculated path
+        _path = p; // Store the calculated path
+        _currentWaypoint = 0; // Reset the current waypoint index to the start
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
-        if (path == null) // Check if there is no path to follow.
+        if (_path == null) // Check if there is no path to follow.
         {
             return;
         }
 
-        if (currentWaypoint >= path.vectorPath.Count) // Check if the spirit has reached the end of the path.
+        if (_currentWaypoint >= _path.vectorPath.Count) // Check if the spirit has reached the end of the path.
         {
-            // reachedEndOfPath = true;
             return;
         }
-        else
-        {
-            // reachedEndOfPath = false;
-        }
-
-        // Calculate the direction and force needed to move towards the current waypoint.
-        Vector2 direction = ((Vector2)path.vectorPath[currentWaypoint] - rb.position).normalized;
+        
+        // Calculate the direction and force needed to move towards the current waypoint
+        var direction = ((Vector2)_path.vectorPath[_currentWaypoint] - _rb.position).normalized;
         speed = UpdateSpeedToTarget();
 
-        Vector2 force = speed * Time.deltaTime * direction;
+        var force = speed * Time.deltaTime * direction;
 
-        float distance = Vector2.Distance(rb.position, path.vectorPath[currentWaypoint]);
+        var distance = Vector2.Distance(_rb.position, _path.vectorPath[_currentWaypoint]);
 
-        rb.AddForce(force); // Apply the calculated force to move the spirit.
+        _rb.AddForce(force); // Apply the calculated force to move the spirit
 
-        if (distance < nextWaypointDistance) // Check if the spirit has reached the current waypoint.
+        if (distance < NextWaypointDistance) // Check if the spirit has reached the current waypoint
         {
-            currentWaypoint++; // Move to the next waypoint in the path.
+            _currentWaypoint++; // Move to the next waypoint in the path
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        // If spirit collides with portal, destroy gameobject.
+        // If spirit collides with portal, destroy game object
         if (collision.gameObject.CompareTag("Portal"))
         {
             Destroy(gameObject);
@@ -151,19 +136,13 @@ public class SoulControl : MonoBehaviour
 
     private float UpdateSpeedToTarget()
     {
-        // If the portal is the closest target.
-        if (target.type == "portal" && target.distance < distanceThreshold)
+        // If the portal is the closest target
+        if (_target.Type == "portal" && _target.Distance < DistanceThreshold)
         {
-            return fastSpeed;
+            return FastSpeed;
         }
-        // If the player is the closest target and the spirit is far away.
-        else if (target.type == "player" && target.distance > distanceThreshold)
-        {
-            return fastSpeed;
-        }
-        else
-        {
-            return normalSpeed;
-        }
+
+        // If the player is the closest target and the spirit is far away
+        return _target is { Type: "player", Distance: > DistanceThreshold } ? FastSpeed : NormalSpeed;
     }
 }
