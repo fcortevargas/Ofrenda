e4ba8e7df38d8edd1c583ce4eae254da7d784205
fcortevargas <f.cortevargas@gmail.com>
2024-01-16 23:58:33 +0100

    :art: Changed the naming of the private variables to adhere with naming conventions and removed unnecessary debug logging.

--- Assets/Scripts/Items/Candle.cs
+++ Assets/Scripts/Items/Candle.cs
@@ -3,30 +3,28 @@ using UnityEngine.Rendering.Universal;
 
 public class Candle : MonoBehaviour
 {
-    private Light2D mainLight;
-    private Light2D fireLight;
-    private Collider2D trigger;
+    private Light2D _mainLight;
+    private Light2D _fireLight;
+    private Collider2D _trigger;
 
     private void Start()
     {
         // Get collider trigger object
-        trigger = GetComponent<Collider2D>();
+        _trigger = GetComponent<Collider2D>();
 
         // Find the Light2D components in the child objects
-        mainLight = transform.Find("Lights/Main Light").GetComponent<Light2D>();
-        fireLight = transform.Find("Lights/Fire Light").GetComponent<Light2D>();
+        _mainLight = transform.Find("Lights/Main Light").GetComponent<Light2D>();
+        _fireLight = transform.Find("Lights/Fire Light").GetComponent<Light2D>();
 
         // Start with the lights off
-        if (mainLight != null)
+        if (_mainLight != null)
         {
-            Debug.Log("Main Light Object Found");
-            mainLight.intensity = 0;
+            _mainLight.intensity = 0;
         }
 
-        if (fireLight != null)
+        if (_fireLight != null)
         {
-            Debug.Log("Fire Light Object Found");
-            fireLight.intensity = 0;
+            _fireLight.intensity = 0;
         }
     }
 
@@ -34,10 +32,9 @@ public class Candle : MonoBehaviour
     {
         if (other.CompareTag("Fire"))
         {
-            Debug.Log("Player triggered object");
             // Turn on the lights fully
-            if (mainLight != null) mainLight.intensity = 1;
-            if (fireLight != null) fireLight.intensity = 0.5f;
+            if (_mainLight != null) _mainLight.intensity = 1;
+            if (_fireLight != null) _fireLight.intensity = 0.5f;
         }
     } 
 }
